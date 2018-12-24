e4c1a5c7bfa1a0fa0bbbee365f436482308e7736
mayurdere <mayur@adolpha.com>
2018-12-24 15:41:43 +0530

    Factory and Seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Model\Category;
+use App\Model\Question;
+use App\Model\Reply;
+use App\Model\Like;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(\App\User::class, 10)->create();
+        factory(\App\Model\Category::class, 5)->create();
+        factory(\App\Model\Question::class, 10)->create();
+        factory(\App\Model\Reply::class, 50)->create()->each(function($reply) {
+            return $reply->like()->save(factory(\App\Model\Like::class)->make());
+        });
     }
 }
