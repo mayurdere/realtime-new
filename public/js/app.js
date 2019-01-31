e8690ca7ddbeb9022a8a6db6546895eaeb77a0d5
mayurdere <mayur@adolpha.com>
2019-01-31 11:10:53 +0530

    65. Delete Question

--- public/js/app.js
+++ public/js/app.js
@@ -2006,6 +2006,17 @@ __webpack_require__.r(__webpack_exports__);
     body: function body() {
       return md.parse(this.data.body);
     }
+  },
+  methods: {
+    destroy: function destroy() {
+      var _this = this;
+
+      axios.delete("/api/question/".concat(this.data.slug)).then(function (res) {
+        return _this.$router.push('/forum');
+      }).catch(function (error) {
+        return console.log(error.response.data);
+      });
+    }
   }
 });
 
@@ -20628,7 +20639,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
 
 // exports
 
@@ -57094,7 +57105,10 @@ var render = function() {
                   _vm._v(" "),
                   _c(
                     "v-btn",
-                    { attrs: { icon: "", small: "" } },
+                    {
+                      attrs: { icon: "", small: "" },
+                      on: { click: _vm.destroy }
+                    },
                     [
                       _c("v-icon", { attrs: { color: "red" } }, [
                         _vm._v("delete")
--- resources/js/components/forum/ShowQuestion.vue
+++ resources/js/components/forum/ShowQuestion.vue
@@ -18,7 +18,7 @@
             <v-btn icon small>
                 <v-icon color="orange">edit</v-icon>
             </v-btn>
-            <v-btn icon small>
+            <v-btn icon small @click="destroy">
                 <v-icon color="red">delete</v-icon>
             </v-btn>
         </v-card-actions>
@@ -39,6 +39,13 @@ export default {
     computed:{
         body(){
             return md.parse(this.data.body)
+        }
+    },
+    methods:{
+        destroy(){
+            axios.delete(`/api/question/${this.data.slug}`)
+            .then(res => this.$router.push('/forum'))
+            .catch(error => console.log(error.response.data))
         }
     }
 }
